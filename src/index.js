f1e6c4fa54d4e8f48e703cd39bd14781ed566343
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-06-02 21:57:26 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,44 +1,39 @@
 import './sass/main.scss';
-import {refs} from './js/variables'; 
+import { refs } from './js/variables';
 import countryCard from './templates/country-card.hbs';
-import countryList from './templates/country-list.hbs'; 
+import countryList from './templates/country-list.hbs';
 // import API from './js/api-service.js'
 // console.log(API)
 
-import fetchCountries from './js/fetchCountries.js'
+import fetchCountries from './js/fetchCountries.js';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import { alert, error, defaultModules } from '@pnotify/core';
-import * as PNotifyMobile from '@pnotify/mobile'; 
+import * as PNotifyMobile from '@pnotify/mobile';
 
 defaultModules.set(PNotifyMobile, {});
 
-// alert({
-//   text: 'Notice me, senpai!'
-// });
-
 const debounce = require('lodash.debounce');
 
-refs.input.addEventListener('input', debounce(onInputValue,500));
+refs.input.addEventListener('input', debounce(onInputValue, 500));
 
-function onInputValue(event){
-    event.preventDefault();
+function onInputValue(event) {
+  event.preventDefault();
 
+  const form = event.target;
+  const searchQuery = form.value.trim();
+  if (searchQuery === '') {
+    return alert({
+      title: 'Хм...',
+      text: 'Дружок-пирожок, так мы ничего не найдем!',
+      delay: 1000,
+    });
+  }
+  // API.fetchCountryBuName(searchQuery)
+  //     .then(renderCountryCard)
+  //     .catch(onFetchError);
 
-const form = event.target;
-const searchQuery = form.value.trim();
-    if(searchQuery === ''){
-        return alert({
-            title: 'Хм...',
-            text: 'Дружок-пирожок, так мы ничего не найдем!',
-            delay: 1000
-        });;
-    }
-// API.fetchCountryBuName(searchQuery) 
-//     .then(renderCountryCard)
-//     .catch(onFetchError);
-
-fetchCountries(searchQuery)
+  fetchCountries(searchQuery)
     .then(renderCountry)
     .catch(error => console.log(error));
 }
@@ -51,39 +46,42 @@ fetchCountries(searchQuery)
 // });
 // }
 
-// //todo отдельная функция для отрисовки разметки карточки стран 
-function renderCountryCard(data){
-    const doCountryCard = countryCard(data);
-    refs.countryCard.innerHTML = doCountryCard;
+// //todo отдельная функция для отрисовки разметки карточки стран
+function renderCountryCard(data) {
+  const doCountryCard = countryCard(data);
+  refs.countryCard.innerHTML = doCountryCard;
 }
 
 // //todo отдельная функция для отрисовки разметки списка стран
-function renderCountryList(data){
-    const doCountryList = countryList(data);
-    refs.listCountry.innerHTML = doCountryList;
+function renderCountryList(data) {
+  const doCountryList = countryList(data);
+  refs.listCountry.innerHTML = doCountryList;
 }
 
-function onFetchError(error){
+function renderCountry(countries) {
+  if (countries.status === 404) {
+    //   return response.json();
+    // } else {
     error({
-        title: "Error:",
-        text: "No country with this name!"
+      title: 'ОШИБКА!',
+      text: 'ТАКОЙ СТРАНЫ НЕТ!',
+      delay: 1500,
     });
-}
+  }
 
-function renderCountry(countries){
-    if(countries.length===1){
-        renderCountryCard(countries);
-        refs.listCountry.innerHTML = '';
-    }
-    if(countries.length >=2 && countries.length<=10){
-        renderCountryList(countries)
-        refs.countryCard.innerHTML = '';
-    }
-    if(countries.length>10){
-        error({
-            title: "Ой-ой, сильно много стран!",
-            text: "Введи боллее валидный запрос!",
-            delay: 1500
-        });
-    }
-}   
+  if (countries.length === 1) {
+    renderCountryCard(countries);
+    refs.listCountry.innerHTML = '';
+  }
+  if (countries.length >= 2 && countries.length <= 10) {
+    renderCountryList(countries);
+    refs.countryCard.innerHTML = '';
+  }
+  if (countries.length > 10) {
+    error({
+      title: 'Ой-ой, сильно много стран!',
+      text: 'Введи боллее валидный запрос!',
+      delay: 1500,
+    });
+  }
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,7 @@
-const BASE_URL = 'https://restcountries.eu/rest/v2'
+const BASE_URL = 'https://restcountries.eu/rest/v2';
 
-export default function fetchCountries(inputName){
-    return fetch (`${BASE_URL}/name/${inputName}`)
-    .then(response=>{
-        // return response.json();
-        if (response.ok) {
-            return response.json();
-            } else {
-                alert("Ошибка HTTP: " + response.status);
-            }
-});
+export default function fetchCountries(inputName) {
+  return fetch(`${BASE_URL}/name/${inputName}`).then(response => {
+    return response.json();
+  });
 }
-
