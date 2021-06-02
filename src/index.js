af174eba60dd7946fec22a72981302575c672adf
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-06-02 14:00:20 +0300

    Сделал условие отрисовки HTML

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
 import './sass/main.scss';
 import {refs} from './js/variables'; 
-import countryCard from './templates/country-card.hbs'; 
+import countryCard from './templates/country-card.hbs';
+import countryList from './templates/country-list.hbs'; 
 // import API from './js/api-service.js'
 // console.log(API)
+
 import fetchCountries from './js/fetchCountries.js'
-// console.log(fetchCountries)
-refs.input.addEventListener('input', onInputValue);
+
+const debounce = require('lodash.debounce');
+
+refs.input.addEventListener('input', debounce(onInputValue,500));
 
 function onInputValue(event){
 event.preventDefault();
 
-const form = event.currentTarget;
-console.log(form.value)
-const searchQuery = form.value;
+const form = event.target;
+const searchQuery = form.value.trim();
 
 // API.fetchCountryBuName(searchQuery) 
 //     .then(renderCountryCard)
 //     .catch(onFetchError);
 
 fetchCountries(searchQuery)
-    .then(renderCountryCard)
+    .then(renderCountry)
     .catch(onFetchError);
 }
-
-
 
 //todo отдельная функция для fetch
 // function fetchCountryBuName(inputName){
@@ -32,12 +33,31 @@ fetchCountries(searchQuery)
 //         return response.json();
 // });
 // }
-//todo отдельная функция для отрисовки разметки по данным 
-function renderCountryCard(country){
-    const doCountryCard = countryCard(country);
+
+// //todo отдельная функция для отрисовки разметки карточки стран 
+function renderCountryCard(data){
+    const doCountryCard = countryCard(data);
     refs.countryCard.innerHTML = doCountryCard;
 }
 
+// //todo отдельная функция для отрисовки разметки списка стран
+function renderCountryList(data){
+    const doCountryList = countryList(data);
+    refs.listCountry.innerHTML = doCountryList;
+}
+
 function onFetchError(error){
-    alert('Нет такой страны')
+    console.log(error)
+}
+
+function renderCountry(countries){
+    if(countries.length===1){
+        renderCountryCard(countries);
+    }
+    if(countries.length >=2 && countries.length<=10){
+        renderCountryList(countries)
+    }
+    if(countries.length>10){
+        alert('Седалайте валидный запрос')
+    }
 }
