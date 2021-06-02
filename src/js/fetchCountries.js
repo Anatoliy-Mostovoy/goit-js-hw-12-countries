5d8a0aef4b9883a401dfadac38f1dfc2a8649d7a
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-06-02 21:04:29 +0300

    Ловим ошибку 404

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,14 @@
 const BASE_URL = 'https://restcountries.eu/rest/v2'
 
 export default function fetchCountries(inputName){
-    return fetch (`${BASE_URL}/name/${inputName}`).then(response=>{
-        return response.json();
+    return fetch (`${BASE_URL}/name/${inputName}`)
+    .then(response=>{
+        // return response.json();
+        if (response.ok) {
+            return response.json();
+            } else {
+                alert("Ошибка HTTP: " + response.status);
+            }
 });
 }
 
